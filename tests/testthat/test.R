f97976a07e722dec45a9c87daa491cf190d1c3dc
Prash Medirattaa <prash16@gmail.com>
2016-12-16 14:17:19 -0800

    Delete test.R

--- tests/testthat/test.R
+++
@@ -1,9 +0,0 @@
-library(circlefunction)
-
-context("Circle function Testing")
-
-test_that("Correct area",{
-	expect_equal(find.diameter(circle(5)),10)
-	expect_equal(find.area(circle(5)),78.53982,tolerance=.2)
-	expect_equal(find.perimeter(circle(5)),31.41593,tolerance=.2)
-})
